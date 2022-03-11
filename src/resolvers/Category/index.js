8bb207d98108120fe24c000d9bb1db752675a87a
Raman Kumar <rks3348@yahoo.com>
2022-03-12 01:40:25 +0530

    updated

--- src/resolvers/Category/index.js
+++ src/resolvers/Category/index.js
@@ -1,4 +1,4 @@
-const books = [
+const categories = [
     {
         id: 0,
         name: 'Phone',
@@ -12,12 +12,19 @@ const books = [
 const CategoryResolvers = {
     Query: {
         categories: (_parent, args, context) => {
-            console.log(context);
-            return books;
+            return categories;
         },
         getCategory: (_parent, args, context) => {
-            return books;
-        },
+            var category = categories[0];
+            return category;
+        }
     },
+    Mutation: {
+        createCategory: async (_parent, args, { dataSources }, info) => {
+            const user = users[0];
+            var token = 'asdasd8sad87as8d7as8dasd74546';
+            return { token, user };
+        }
+    }
 };
 export default CategoryResolvers;
--- src/resolvers/User/index.js
+++ src/resolvers/User/index.js
@@ -3,20 +3,30 @@ const users = [
         id: 1,
         username: 'xyz',
         email: 'rks3348@gmail.com',
+        firstname: 'firstname',
+        lastname: 'firstname'
     },
     {
         id: 2,
         username: 'abc',
         email: 'rks@gmail.com',
-    },
+        firstname: 'firstname',
+        lastname: 'firstname'
+    }
 ];
 const UserResolver = {
     Query: {
-        users: async ( _parent, args, { dataSources }, info ) => {
+        users: async (_parent, args, { dataSources }, info) => {
             let posts = await dataSources.CMSApi._('users').getAll(1);
-            console.log(posts);
             return posts;
-        },
+        }
+    },
+    Mutation: {
+        signup: async (_parent, args, { dataSources }, info) => {
+            const user = users[0];
+            var token = 'asdasd8sad87as8d7as8dasd74546';
+            return { token, user };
+        }
     }
 };
 export default UserResolver;
